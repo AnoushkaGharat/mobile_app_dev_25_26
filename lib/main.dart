7b2a556cbc640371ed350d9b0f3cc67edee57aee
AnoushkaGharat <83427391+AnoushkaGharat@users.noreply.github.com>
2026-04-19 15:49:07 -0700

    Made progress on home page

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MainPage(title: 'Flutter Demo Home Page'),
     );
   }
 }
@@ -120,6 +120,101 @@ class _MyHomePageState extends State<MyHomePage> {
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+
+
+class _MainPageState extends State<MainPage> {
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // TRY THIS: Try changing the color here to a specific color (to
+        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
+        // change color while the other colors stay the same.
+        backgroundColor: const Color.fromARGB(255, 120, 142, 221),//Theme.of(context).colorScheme.inversePrimary,
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: const Text("FBLA Application", style: TextStyle(fontFamily: "Apercu Pro", fontWeight: FontWeight.bold, color: const Color.fromARGB(255, 255, 255, 255)))
+        ),
+        body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        
+        //Full page
+        child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          //
+          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
+          // action in the IDE, or press "p" in the console), to see the
+          // wireframe for each widget.
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+
+            //Top area
+            Stack( 
+              alignment: Alignment.center, 
+              children:<Widget>[
+                Text(
+                    'Hello',
+                    style: Theme.of(context).textTheme.headlineMedium,
+                  ),
+              ],
+            ),
+            Stack(
+                alignment: Alignment.center, 
+                children: <Widget>[
+                  const Padding(
+                      padding: EdgeInsets.only(bottom:50.0), // Adds 16px space on all sides
+                      child: Text("Your Text Here"),
+                    ),
+                  Text(
+                    'Hello',
+                    style: Theme.of(context).textTheme.headlineMedium,
+                  ),
+                ],
+                )
+          ]
+        )
+      )
     );
   }
 }
